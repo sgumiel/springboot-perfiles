3dd56485e3ceca4bad958b3354c88da9d6fa7c56
Sergio Gumiel <sergio.gumiel@kairosds.com>
2022-02-10 20:39:11 +0100

    First commit

---
+++ src/test/java/com/kairosds/cursospb2/perfiles/PerfilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kairosds.cursospb2.perfiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
